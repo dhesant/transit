e03802ed1be382d61e2990b1b2049d7bd563354a
Dhesant Nakka <dhesant@gmail.com>
2017-12-26 18:58:24 +0800

    fixed issue where program is not setting the route correctly

--- nextbus.py
+++ nextbus.py
@@ -4,37 +4,51 @@ from bs4 import BeautifulSoup
 import datetime
 import dateutil.parser as dparser
 
-# Map bus routes to system ID's
-routeid = { "8X": '56', "19": '191' }
-operator = { "8X": 'citybus', "19": 'citybus' }
+# Map bus routes to system ID"s
+operator = { "8x": "citybus", "19": "citybus" }
+stopcode = { "8x": "002552||8X-ISR-1||3||I", "19": "002552||19-ISR-1||8||I" }
 
 # Create session cookie for HTML request
-ssidcookie = {"5a41dea32347b": 'lpb8m4omcdcrk2fdm501g1h9p3'}
+ssid = "5a41dea32347b"
+ssidcookie = { ssid: "lpb8m4omcdcrk2fdm501g1h9p3"}
 
-# Get HTML from NWST
-def getRawBuses(routeid):
-    r = requests.get("https://mobile.nwstbus.com.hk/nwp3/geteta.php?l=1&ssid=5a41dea32347b&sysid=" + routeid, cookies=ssidcookie)
+# Get raw data from nwstbus.com.hk
+def getRawBuses(route):
+    payload = { "info": stopcode[route], "ssid": ssid, "sysid": 34 }
+    r = requests.get("https://mobile.nwstbus.com.hk/nwp3/set_etasession.php", params=payload, cookies=ssidcookie)
+    if (r.text != "OK"):
+        return None
+
+    payload = { "l": "1", "ssid": ssid, "sysid": 34 }
+    r = requests.get("https://mobile.nwstbus.com.hk/nwp3/geteta.php", params=payload, cookies=ssidcookie)
+    if (r.text == ":("):
+        return None
+
     return r.text
 
-# Parse 
+# Get list of next buses
 def getBuses(route):
-    raw = getRawBuses(routeid[route])
-    
-    if (raw == ":("):
+    # Sanitize route number to lowercase
+    route = route.lower()
+
+    # Get and verify raw data from nwstbus.com.hk
+    raw = getRawBuses(route)
+    if (raw == None):
         return None
-    
+
+    # Parse results into a dictionary
     soup = BeautifulSoup(raw, "lxml");
     
     results = {"children": []}
     
     for child in soup.find(id="nextbux_list").children:
         results["children"].append({
-            'route': soup.find(id="nextbus_title").find('tr').contents[1].string.upper(),
-            #'route': route,
-            'operator': operator[route],
-            'eta': dparser.parse(child.find_all("table")[0].td.string),
-            'dest': child.find_all("table")[1].find_all('td')[0].string.replace("To: ", "" ,1),
-            'isArrived': (True if child.find_all("table")[1].find_all('td')[1].string == "Arrived" else False)
+            "route": soup.find(id="nextbus_title").find("tr").contents[1].string.upper(),
+            #"route": route,
+            "operator": operator[route],
+            "eta": dparser.parse(child.find_all("table")[0].td.string),
+            "dest": child.find_all("table")[1].find_all("td")[0].string.replace("To: ", "" ,1),
+            "isArrived": (True if child.find_all("table")[1].find_all("td")[1].string == "Arrived" else False)
         })
 
     return results
