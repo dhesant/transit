113615c32dcea61c62314f2ee01683f86b475d26
Dhesant Nakka <dhesant@gmail.com>
2017-12-27 20:48:30 +0800

    updated flask with initial template and coding

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 # from http://flask.pocoo.org/ tutorial
 from flask import Flask, render_template
+from datetime import datetime, timedelta
 app = Flask(__name__)
 
-@app.route("/") # take note of this decorator syntax, it's a common pattern
-def hello():
-    return "Hello World!"
+from nexttransit import getTransitHVT
 
-@app.route('/welcome')
-def welcome():
-        return render_template('welcome.html')
+@app.route("/") # take note of this decorator syntax, it's a common pattern
+def index():
+    return render_template("index.html")
+
+@app.route("/hvt")
+def hvt():
+    vehicles = sorted(getTransitHVT(), key=lambda k: k['eta'])
+
+    now = datetime.today()
+    for vehicle in vehicles:
+        if vehicle['isArrived']:
+            vehicle['status'] = "Arrived"
+        else:
+            dtime = vehicle['eta'] - now
+            vehicle['status'] = "In " + str(int(dtime.total_seconds() / 60)) + " Minutes"
+    
+    return render_template("transit.html", title="Happy Valley", stopinfo="Tram Terminus/Sanatorium", vehicles=vehicles)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
 
